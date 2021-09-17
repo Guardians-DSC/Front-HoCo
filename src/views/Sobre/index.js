2ca4b11d25ac83ae267289ffe88da1adeec74ea0
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-09-17 11:14:58 -0300

    Remove import feito na pagina de sobre

--- src/views/Sobre/index.js
+++ src/views/Sobre/index.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import { ActivitiesTable } from '../../components/ActivitiesTable'
 
 export const Sobre = () => {
-  return (
-    <div>
-      <div styled={{ width: '80%' }}>
-        <ActivitiesTable />
-      </div>
-    </div>
-  )
+  return <div>Sobre</div>
 }
