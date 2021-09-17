94b554297038f0dd960aa00c17591108c7a41c17
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-09-17 20:31:28 -0300

    adiciona serviço para acesso a API do github

---
+++ src/services/github.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com';
+
+const APIGithub = axios.create({
+	baseURL: GITHUB_API_URL,
+	headers: {
+		'Content-Type': 'application/json'
+	},
+});
+
+export const github = {
+	async getAllRepositoriesFromOrganization() {
+		const response = await APIGithub.get('/orgs/guardians-dsc/repos');
+		return response.data;
+	},
+	async getContributorsFromRepository(repositoryName) {
+		const response = await APIGithub.get(
+			`/repos/${repositoryName}/contributors`
+		);
+		return response;
+	},
+	async getAllHocoContributors(repositoriesNames) {
+		let contributors = await Promise.all(
+			repositoriesNames.map((name) => this.getContributorsFromRepository(name))
+		).then((values) => {
+			const allUsers = [].concat(...values.map((value) => [...value.data]));
+
+			const users = {};
+			allUsers.map((user) => users[user.login] = {
+				imageUrl: user.avatar_url,
+				profileUrl: user.html_url,
+			});
+
+			return users;
+		});
+
+		return contributors;
+	}
+};
