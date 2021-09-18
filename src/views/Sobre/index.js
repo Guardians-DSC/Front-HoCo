384cc4d1cba9870ad27742cd8a76378cf3b6065d
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-09-18 14:07:46 -0300

    modifica utilização de texto para strings javascript

--- src/views/Sobre/index.js
+++ src/views/Sobre/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Contributors } from '../../components/Contributors'
 import { ShortLogo } from '../../assets/icons/ShortLogo'
 
-import { Page, Content, Title, Subtitle } from '../common'
+import { Page, Content, Title, Subtitle, Text } from '../base-styles.js'
 
 export const Sobre = () => {
   return (
@@ -11,44 +11,45 @@ export const Sobre = () => {
       <Content>
         <Title>
           <ShortLogo width={'3rem'} color={'#514E4E'} />
-          <h1>Sobre o HoCo</h1>
+          <h1>{`Sobre o HoCo`}</h1>
         </Title>
-        <p>
-          Olá! o Hoco é uma plataforma idealizada por alunos da graduação com o
-          objetivo de ajudar os alunos da graduação de ciência da computação da
-          Universidade Federal de Campina Grande (UFCG) a gerenciarem as suas
-          atividades complementares.
-        </p>
-        <p>
-          A ideia do HoCo é proporcionar uma plataforma unificada onde é possível
+        <Text>
+          {`Olá! o Hoco é uma plataforma idealizada por alunos da graduação com
+            o objetivo de ajudar os alunos da graduação de ciência da computação
+            da Universidade Federal de Campina Grande (UFCG) a gerenciarem as 
+            suas atividades complementares.`}
+        </Text>
+        <Text>
+          {`A ideia do HoCo é proporcionar uma plataforma unificada onde é possível `}
           <span>
-            armazenar, visualizar o progresso das suas horas complementares, tirar
-            dúvidas acerca do tema
+            {`armazenar, visualizar o progresso das suas horas complementares, tirar
+            dúvidas acerca do tema`}
           </span>
-          e também <span>ficar atento quanto às atividades extra-curriculares</span>
-          que o curso oferece.
-        </p>
-        <Subtitle>O que preciso ter e fazer para contribuir?</Subtitle>
-        <p>Você pode contribuir de diversas formas!</p>
-        <p>
-          Caso você queira contribuir através de código você pode acessar os{' '}
+          {`e também`}
+          <span>{`ficar atento quanto às atividades extra-curriculares`}</span>
+          {`que o curso oferece`}.
+        </Text>
+        <Subtitle>{`O que preciso ter e fazer para contribuir?`}</Subtitle>
+        <Text>{`Você pode contribuir de diversas formas!`}</Text>
+        <Text>
+          {`Caso você queira contribuir através de código você pode acessar os `}
           <a
             href="https://github.com/Guardians-DSC/HoCo"
             target="_blank"
             rel="noreferrer"
           >
-            repositórios do HoCo no gitHub
+            {`repositórios do HoCo no gitHub`}
           </a>
-          , ver quais as issues que estão cadastradas nos repositórios, e ler os
+          {`, ver quais as issues que estão cadastradas nos repositórios, e ler os
           documentos de código de conduta e contribuição para entender como você pode
-          ajudar na melhoria da plataforma.
-        </p>
-        <p>
-          Se você tiver alguma ideia de funcionalidade para o HoCo você pode
+          ajudar na melhoria da plataforma.`}
+        </Text>
+        <Text>
+          {`Se você tiver alguma ideia de funcionalidade para o HoCo você pode
           participar das nossas reuniões abertas no discord do curso, onde nós
-          discutimos o nosso progresso e futuro do projeto.
-        </p>
-        <Subtitle>Pessoas que contribuiram na elaboração do projeto</Subtitle>
+          discutimos o nosso progresso e futuro do projeto.`}
+        </Text>
+        <Subtitle>{`Pessoas que contribuiram na elaboração do projeto`}</Subtitle>
         <Contributors />
       </Content>
     </Page>
