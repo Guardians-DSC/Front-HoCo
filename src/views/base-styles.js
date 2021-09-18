213c1f51a00b927560222f9a7a9dc2bb95c6ddd6
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-09-18 14:07:05 -0300

    modifica nome do arquivo de estilos das páginas e aplica alterações sugeridas

--- src/views/base-styles.js
+++ src/views/base-styles.js
@@ -5,40 +5,41 @@ export const Page = styled.div`
 `
 
 export const Content = styled.div`
-	width: auto;
-			
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
+  width: auto;
 
-	margin: 2rem 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 
-	color: #514E4E;
+  margin: 3.5rem 2.5rem;
 
-	& > p {
-		font-size: 1.25rem;
+  color: #514e4e;
+`
 
-		& > span {
-			font-weight: bold;
-		}
+export const Text = styled.p`
+  font-size: 1rem;
 
-		& > a {
-			color: #514E4E;
-			font - weight: bold;
-			text - decoration: underline;
-			
-			&:active, &visited {
-				color: #514E4E;
-			}
-		}
-	}
+  & > span {
+    background: #dfdfdf;
+  }
+
+  & > a {
+    color: #514e4e;
+    font-weight: bold;
+    text-decoration: underline;
+
+    &:active,
+    &visited {
+      color: #514e4e;
+    }
+  }
 `
 
 export const Title = styled.h1`
   display: flex;
   gap: 1rem;
   align-items: center;
-  margin-bottom: 1rem;
+  margin-bottom: 2rem;
   font-weight: 500;
   color: #514e4e;
   letter-spacing: 0.1rem;
@@ -50,10 +51,9 @@ export const Subtitle = styled.h2`
   display: flex;
   gap: 1rem;
   align-items: center;
-  margin-bottom: 1rem;
-  margin: 1rem 0;
+  margin: 1.5rem 0;
   font-weight: 600;
   color: #514e4e;
 
-  font-size: 1.75rem;
+  font-size: 1.5rem;
 `
