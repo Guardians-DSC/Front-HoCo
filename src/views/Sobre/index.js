6052800fa1a26edf853d09063432257ace1bff99
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-09-17 23:41:50 -0300

    corrige erros de linter e modifica import de arquivo de estilos

--- src/views/Sobre/index.js
+++ src/views/Sobre/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
-import { Contributors } from '../../components/Contributors';
-import { ShortLogo } from '../../assets/icons/ShortLogo';
+import { Contributors } from '../../components/Contributors'
+import { ShortLogo } from '../../assets/icons/ShortLogo'
 
-import {
-  Page,
-  Content,
-  Title,
-  Subtitle,
-} from '../commom'
+import { Page, Content, Title, Subtitle } from '../common'
 
 export const Sobre = () => {
   return (
@@ -19,42 +14,43 @@ export const Sobre = () => {
           <h1>Sobre o HoCo</h1>
         </Title>
         <p>
-          Olá! o Hoco é uma plataforma idealizada por alunos da graduação
-          com o objetivo de ajudar os alunos da graduação de ciência da
-          computação da Universidade Federal de Campina Grande (UFCG) a
-          gerenciarem as suas atividades complementares.
+          Olá! o Hoco é uma plataforma idealizada por alunos da graduação com o
+          objetivo de ajudar os alunos da graduação de ciência da computação da
+          Universidade Federal de Campina Grande (UFCG) a gerenciarem as suas
+          atividades complementares.
         </p>
         <p>
-          A ideia do HoCo é proporcionar uma plataforma unificada onde é
-          possível <span>armazenar, visualizar o progresso das suas horas
-            complementares, tirar dúvidas acerca do tema</span> e também <span>
-            ficar atento quanto às atividades extra-curriculares</span> que o
-          curso oferece.
+          A ideia do HoCo é proporcionar uma plataforma unificada onde é possível
+          <span>
+            armazenar, visualizar o progresso das suas horas complementares, tirar
+            dúvidas acerca do tema
+          </span>
+          e também <span>ficar atento quanto às atividades extra-curriculares</span>
+          que o curso oferece.
         </p>
-        <Subtitle>
-          O que preciso ter e fazer para contribuir?
-        </Subtitle>
-        <p>
-          Você pode contribuir de diversas formas!
-        </p>
+        <Subtitle>O que preciso ter e fazer para contribuir?</Subtitle>
+        <p>Você pode contribuir de diversas formas!</p>
         <p>
           Caso você queira contribuir através de código você pode acessar os{' '}
-          <a href="https://github.com/Guardians-DSC/HoCo" className="paragraphLink" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/Guardians-DSC/HoCo"
+            target="_blank"
+            rel="noreferrer"
+          >
             repositórios do HoCo no gitHub
-          </a>, ver quais as issues que estão cadastradas
-          nos repositórios, e ler os documentos de código de conduta e contribuição
-          para entender como você pode ajudar na melhoria da plataforma.
+          </a>
+          , ver quais as issues que estão cadastradas nos repositórios, e ler os
+          documentos de código de conduta e contribuição para entender como você pode
+          ajudar na melhoria da plataforma.
         </p>
         <p>
           Se você tiver alguma ideia de funcionalidade para o HoCo você pode
-          participar das nossas reuniões abertas no discord do curso, onde
-          nós discutimos o nosso progresso e futuro do projeto.
+          participar das nossas reuniões abertas no discord do curso, onde nós
+          discutimos o nosso progresso e futuro do projeto.
         </p>
-        <Subtitle>
-          Pessoas que contribuiram na elaboração do projeto
-        </Subtitle>
+        <Subtitle>Pessoas que contribuiram na elaboração do projeto</Subtitle>
         <Contributors />
       </Content>
     </Page>
-  );
+  )
 }
