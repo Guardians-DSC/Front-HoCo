2dbff43d26b65ba0ccd0b6e2cf0147296b80f7fb
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-09-23 00:15:37 -0300

    adiciona página de organizações

--- src/views/Organizacoes/index.js
+++ src/views/Organizacoes/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import { Organizations } from '../../components/Organizations'
+
+import { Content, Title, Text } from '../../styles/base-styles.js'
+
 export const Orgs = () => {
-  return <div>Orgs</div>
+  return (
+    <Content>
+      <Title>Organizações</Title>
+      <Text>
+        {`Olá, querido usuário! Para que você saiba como ganhar algumas horinhas
+        complementares fique de olho nessas organizações. Além de aprender muita 
+        coisa massa, participar de eventos, palestras, mini-cursos, etc, 
+        também te garante algumas horinhas extras.`}
+      </Text>
+      <Organizations />
+    </Content>
+  )
 }
